24f1dcf624a80845b55fa4db24125cf43b2e6f84
TheSoulrester <alex.gropp@icloud.com>
2022-10-05 17:34:36 +0200

    group_id => vererben mit groups-DB

--- database/migrations/2022_10_05_142326_create_users_table.php
+++ database/migrations/2022_10_05_142326_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->foreignID('group_id')->nullable(false);
+            $table->foreignID('group_id')->nullable(false)->constrained('groups')->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('name');
