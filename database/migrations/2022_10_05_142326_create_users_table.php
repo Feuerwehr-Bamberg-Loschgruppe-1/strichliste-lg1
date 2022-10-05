c2b3936868a2e5cf646ac496dad37aa4e1fb9bbb
TheSoulrester <alex.gropp@icloud.com>
2022-10-05 17:45:04 +0200

    "available_credit" aus Users-DB entfernt

--- database/migrations/2022_10_05_142326_create_users_table.php
+++ database/migrations/2022_10_05_142326_create_users_table.php
@@ -23,7 +23,6 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->decimal('available_credit');
             $table->timestamps();
         });
     }
